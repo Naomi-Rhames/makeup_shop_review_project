68d996ff5d2027a5f19199764f33a967a12b8bc4
Naomi-Rhames <naomirhames2020@gmail.com>
2021-05-02 18:17:33 -0500

    first commit

--- app/models/makeup.rb
+++ app/models/makeup.rb
@@ -1,7 +1,5 @@
 class Makeup < ActiveRecord::Base
      has_many :reviews  
     
-   def self.search(search)
-    where('name like :pat or content like :pat', :pat => "%#{search}%")
-    end
+
 end
